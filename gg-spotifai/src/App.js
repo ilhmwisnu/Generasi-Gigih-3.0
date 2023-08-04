81a6122d1258cfe9ccbe19eb6155be73563dae67
ilhmwisnu <ilhamwisnu42@gmail.com>
2023-08-04 19:23:23 +0700

    add search feature

--- gg-spotifai/src/App.js
+++ gg-spotifai/src/App.js
@@ -1,35 +1,39 @@
 import LoginView from "./component/login_view";
 import HomeView from "./component/home_view";
 import { useState, useEffect } from "react";
+import {TokenContext} from "./context/context"
 
 function App() {
   let [isLogin, setIsLogin] = useState(false);
   let [accessToken, setAccessToken] = useState(null);
 
   async function login() {
-    setIsLogin(true);
+    window.location =
+      "https://accounts.spotify.com/authorize?client_id=bb9769557ac24a278ebe08db49e4dc22&redirect_uri=http://localhost:3000/&response_type=token&scope=playlist-modify-private";
   }
 
   useEffect(() => {
-    const hashParam = new URLSearchParams(window.location.hash.replace("#",""))
-    
-    let token = hashParam.get("access_token")
+    const hashParam = new URLSearchParams(
+       window.location.hash.replace("#", "")
+    );
+
+    let token = hashParam.get("access_token");
     console.log(token);
 
     if (token) {
-      setAccessToken(token)
-      setIsLogin(true)
+      setAccessToken(token);
+      setIsLogin(true);
     } else {
-      setAccessToken(null)
-      setIsLogin(false)
+      setAccessToken(null);
+      setIsLogin(false);
     }
   }, []);
 
   return (
-    <>
+    <TokenContext.Provider value={accessToken}>
       {isLogin && <HomeView></HomeView>}
       {!isLogin && <LoginView onClick={login}></LoginView>}
-    </>
+    </TokenContext.Provider>
   );
 }
 
--- gg-spotifai/src/component/song_card.jsx
+++ gg-spotifai/src/component/song_card.jsx
@@ -1,14 +1,14 @@
-const SongCard = () => {
+const SongCard = ({title, artist, imgUrl, onClick}) => {
   return (
     <div class="song">
       <div class="flex">
-        <img src="https://picsum.photos/200" width="48" alt="" srcset="" />
+        <img src={imgUrl} width="48" alt="" srcset="" />
         <div class="">
-          <p class="title">Song Title</p>
-          <p class="artist">Song Artist</p>
+          <p class="title">{ title }</p>
+          <p class="artist">{ artist }</p>
         </div>
       </div>
-      <div class="btn">+</div>
+      <div onClick={onClick} class="btn">+</div>
     </div>
   );
 };
