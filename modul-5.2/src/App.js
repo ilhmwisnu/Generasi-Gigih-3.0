4a01a070a7b280377ec959f21daa0a5250a32df9
ilhmwisnu <ilhamwisnu42@gmail.com>
2023-08-04 19:50:46 +0700

    add routing

--- modul-5.2/src/App.js
+++ modul-5.2/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import CurrenConverter from "./view/currency_converter";
+import WeighConverter from "./view/weight_converter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/money">Money</Link>
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <WeighConverter></WeighConverter>
+        </Route>
+        <Route exact path="/money">
+          <CurrenConverter></CurrenConverter>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
